8a39a0bb5fda7eeff1e5784d6fb39207aacb63aa
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-17 03:17:33 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -6,7 +6,11 @@ export default class Invitation extends React.Component{
 
     return(
       <div>
+<<<<<<< HEAD
         <h1>You've been invited!</h1>
+=======
+        <h1> Youve been invited! </h1>
+>>>>>>> 86e5e09aef58ba6c95b19580f21c49bb1ab366fb
           {this.props.children}
       </div>
     )
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> 86e5e09aef58ba6c95b19580f21c49bb1ab366fb
 import React from 'react'
 
 export default class ThemedDecorations extends React.Component{
@@ -7,13 +10,26 @@ export default class ThemedDecorations extends React.Component{
       this.props.children, (child, i) => {
         return (
           React.cloneElement( child, {
+<<<<<<< HEAD
           	className: this.props.theme
           })
+=======
+          	decorationNum: i
+          }),
+
+          <div className="some-component-special-class"> {child} </div>
+>>>>>>> 86e5e09aef58ba6c95b19580f21c49bb1ab366fb
         )
       })
 
       return(
+<<<<<<< HEAD
         <div>
+=======
+        <div className="some-component">
+          <p> This component has {React.Children.count(
+            this.props.children)} children. </p>
+>>>>>>> 86e5e09aef58ba6c95b19580f21c49bb1ab366fb
             {childrenWithWrapperDiv}
         </div>
           )}
